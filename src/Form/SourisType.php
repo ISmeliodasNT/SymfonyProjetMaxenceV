8a8ac76da52da83a7a47bc4bf0b12d7b915f4cbe
Maxence Venner <maxvenner57@gmail.com>
2025-12-05 17:23:19 +0100

    Mise a jour de l'affichage des souris

--- src/Form/SourisType.php
+++ src/Form/SourisType.php
@@ -3,9 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Souris;
+use App\Form\ImageType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Enum\Status;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class SourisType extends AbstractType
 {
@@ -15,11 +19,22 @@ class SourisType extends AbstractType
             ->add('marque')
             ->add('nom')
             ->add('prix')
-            ->add('description')
             ->add('stock')
-            ->add('status')
+            ->add('status', EnumType::class, [
+                'class' => Status::class,
+                'choice_label' => fn(Status $s) => $s->name,
+            ])
             ->add('connectivite')
             ->add('NbBoutons')
+            ->add('description')
+            ->add('images', CollectionType::class, [
+                'entry_type' => ImageType::class,
+                'entry_options' => ['label' => false],
+                'allow_add' => true,    
+                'allow_delete' => true, 
+                'by_reference' => false,
+                'label' => 'Images du produit',
+            ])
         ;
     }
 
